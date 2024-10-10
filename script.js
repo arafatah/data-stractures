eac8fc093048a4f7ae942f3bf0cb4ad96d21883b
arafatah <arafathowlader456@gmail.com>
2024-10-10 12:10:45 +0600

    Challenge 2

--- script.js
+++ script.js
@@ -314,10 +314,9 @@ for (const [i, el] of menu.entries()) {
   // console.log(`${item[0] +1}: ${item[1]}`);
   console.log(`${i +1}: ${el}`);
 }
-*/
 
 if (restaurant.openingHours && restaurant.openingHours.mon)
-  console.log(restaurant.openingHours.mon.open);
+console.log(restaurant.openingHours.mon.open);
 
 // With optional chaining
 console.log(restaurant.openingHours.mon?.open);
@@ -355,5 +354,115 @@ const entries = Object.entries(openingHours)
 console.log(entries); 
 
 for (const [key, {open, close}] of entries){ 
- console.log(`One ${key} we are open at ${open} and close at ${close}`)
+  console.log(`One ${key} we are open at ${open} and close at ${close}`)
+}
+*/
+
+/*
+Coding Challenge #2
+Let's continue with our football betting app! Keep using the 'game' variable from 
+before.
+Your tasks:
+1. Loop over the game.scored array and print each player name to the console, 
+along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already 
+studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them 
+(except for "draw"). Hint: Note how the odds and the game objects have the 
+same property names �
+4. Bonus: Create an object called 'scorers' which contains the names of the 
+players who scored as properties, and the number of goals as the value. In this 
+game, it will look like this:
+{
+ Gnarby: 1,
+ Hummels: 1,
+ Lewandowski: 2
+}
+GOOD LUCK �
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+const entries = Object.entries(game.scored);
+console.log(entries);
+
+for (const i of entries) {
+  console.log(`Goal ${parseInt(i[0]) + 1}: ${i[1]}`);
+}
+
+// Second method;
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}:${player}`);
+}
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+// console.log(team1, draw, team2);
+
+const calcOdds = function (t1, d, t2) {
+  const avrOdds = (t1 + t2 + d) / 3;
+  console.log(avrOdds);
+  return avrOdds;
+};
+calcOdds(team1, team2, draw);
+
+// Second methode 
+const odds = Object.values(game.odds); 
+let  average = 0; 
+for(const odd of odds) 
+  average +=odd; 
+  average /= odds.length; console.log(average); 
+
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them (except for "draw"). Hint: Note how the odds and the game objects have the same property names �
+for(const [team, odd] of Object.entries(game.odds)){
+  const teamStr = team === 'x' ? 'draw' : `Victory ${game[team]}`
+  console.log(`Odd of ${teamStr}: ${odd}`)
 }
