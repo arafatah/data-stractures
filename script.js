87ca21fa58a52d0ddfb121c050d6552ce89014e6
arafatah <arafathowlader890@gmail.com>
2025-01-07 15:35:41 +0600

    set method

--- script.js
+++ script.js
@@ -485,7 +485,7 @@ for (const i of entries) {
 for (const [i, player] of game.scored.entries()) {
   console.log(`Goal ${i + 1}:${player}`);
 }
-// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+// 2. Use a loop to calculate the average odd and log it to the console (already studied how to calculate averages, go check if you don't remember)
 const {
   odds: { team1, x: draw, team2 },
 } = game;
@@ -867,3 +867,73 @@ for (const flight of flights.split('+')) {
   )})`.padStart(35);
   console.log(output);
 }
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+// console.log(team1, draw, team2);
+
+const calcOdds = function (t1, d, t2) {
+  const val = [t1, t2, d];
+  const avrOdds = (t1 + t2 + d) / val.length;
+  console.log(avrOdds);
+  return avrOdds;
+};
+calcOdds(team1, team2, draw);
+
+// Second way. Esiest way.
+const odds = Object.values(game.odds);
+let average = 0;
+
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
+
+// Print odds number
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `Victory ${game[team]}`;
+  console.log(`Odd of ${teamStr}: ${odd}`);
+}
+  
+
